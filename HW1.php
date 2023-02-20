2b2521eb3b08e8396f33500a1c9037eee49d7a70
Cjoechen <125528558+Cjoechen@users.noreply.github.com>
2023-02-20 22:06:22 +0800

    Add files via upload

---
+++ HW1.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+<title>a1103323</title>
+
+</head>
+
+<body>
+Hello HTML!</br>
+
+<?php
+
+echo "Hello PHP";
+
+$i=11;
+$j=18;
+$x=$i*$j;
+echo "<br/>";
+echo $x;
+
+?>
+
+</body>
+
+</html>
